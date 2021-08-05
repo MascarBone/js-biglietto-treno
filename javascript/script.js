80a7400783262f5dc2f37aedf7adb6ae06ddcfa1
MascarBone <simone.bonelli@gmail.com>
2021-08-05 15:05:45 +0200

    Biglietto Treno page created

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// valore del prezzo del biglietto calcolato ogni km
+const prezzoBiglietto = 0.21;
+// Percentuale di sconto da applicare nel caso dell'acquisto di un minorenne
+const discountMinor = 20;
+// Percentuale di sconto da applicare nel caso dell'acquisto di un over 65enne
+const dicsountOver65 = 40;
+
